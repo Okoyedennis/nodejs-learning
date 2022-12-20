701cbf9fc3fe516b83a71db570c918c6b1c7c478
Dennis <okoyedennis7@gmail.com>
2022-12-20 14:15:34 +0100

    template engines

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const express = require("express");
-const adminRoutes = require("./routes/admin")
+const adminData = require("./routes/admin")
 const shopRoutes = require("./routes/shop")
 const bodyParser = require("body-parser");
+// const expressHbs = require("express-handlebars")
 const path = require("path");
 
 
 const app = express();
 
+
+// Template engine for HBS
+// app.engine("hbs", expressHbs({layoutDir: "views/layouts/", defaultLayout: "main-layout", extname:"hbs"}))
+// app.set("view engine", "hbs");
+
+// Template engine for PUG
+app.set("view engine", "pug")
+app.set("views", "views")
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, "public")))
 
-app.use("/admin", adminRoutes)
-app.use(shopRoutes)
+app.use("/admin", adminData.router)
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, "./", "views", "not-found.html"));
+    // res.status(404).sendFile(path.join(__dirname, "./", "views", "not-found.html"));
+    res.status(404).render("404", { docTitle: "Not Found!" });
 })
 
 
